c81f8799efca3291f4549d7312502d3246c6af8f
RasulaMakbul <emu.cfs@gmail.com>
2023-12-20 21:01:34 +0600

    minor bug fix

--- react/views/Signup.jsx
+++ react/views/Signup.jsx
@@ -1,4 +1,5 @@
 import { BeakerIcon } from '@heroicons/react/24/solid';
+import { Link } from 'react-router-dom';
 
 export default function Signup() {
   return (
@@ -53,14 +54,6 @@ export default function Signup() {
               >
                 Password
               </label>
-              <div className="text-sm">
-                <a
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
             </div>
             <div className="mt-2">
               <input
@@ -73,25 +66,44 @@ export default function Signup() {
               />
             </div>
           </div>
-
           <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password-conirmation"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Confirm Password
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password-conirmation"
+                name="password_conirmation"
+                type="password"
+                required
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between mt-2">
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-3 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
-              Sign in
+              Sign Up
             </button>
           </div>
         </form>
 
         <p className="mt-10 text-center text-sm text-gray-500">
           Already have an account?{' '}
-          <a
-            href="#"
+          <Link
+            to="/login"
             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
           >
             Sign in
-          </a>
+          </Link>
         </p>
       </div>
     </>
